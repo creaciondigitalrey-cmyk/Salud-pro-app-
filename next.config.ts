bcd740d302859c970b61a32d11b7348bd73d9c82
creaciondigitalrey-cmyk <creaciondigitalrey@gmail.com>
2026-06-25 08:21:10 -0400

    fijar ruta base para github pages

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = { output: "export", images: { unoptimized: true },
+const nextConfig: NextConfig = { output: "export", images: { unoptimized: true }, basePath: "/Salud-pro-app-", assetPrefix: "/Salud-pro-app-",
   reactStrictMode: false,
   typescript: {
     ignoreBuildErrors: true,
